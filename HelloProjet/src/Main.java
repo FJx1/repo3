f9015b8b04171893070279ca55999bb0047226a1
Fjx <917756350@qq.com>
2018-12-06 20:37:52 +0800

    修改再次提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,4 +6,8 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
     }
+
+    public static void main(String[] args) {
+        System.out.println(".............");
+    }
 }
